2704625912c73e42b9f8649cf8db202e59d6d924
Gagireddy Nithin <gagireddynithinreddy@gmail.com>
2026-06-21 14:44:01 +0530

    Added protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import ReferralDetails from "./pages/ReferralDetails";
-import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/referrals/:id" element={<ReferralDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+<Route
+  path="/"
+  element={
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  }
+/>
