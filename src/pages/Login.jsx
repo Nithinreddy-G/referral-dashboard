fdd2f9b97727b816d288c26daed8edce4f639c2a
Gagireddy Nithin <gagireddynithinreddy@gmail.com>
2026-06-21 15:30:49 +0530

    Fixed Login export

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,30 @@
-const handleLogin = () => {
-  localStorage.setItem("token", "loggedin");
-  navigate("/");
-};
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    localStorage.setItem("token", "loggedin");
+    navigate("/");
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Login Page</h1>
+
+      <input placeholder="Email" />
+      <br />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+      <br />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
